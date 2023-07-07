bd0c7b7ba92bbd069131a4468a915e9bfe526daf
marinacjensenn <84468498+marinacjensenn@users.noreply.github.com>
2023-07-07 14:11:19 +0000

    .

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestHome(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_home(self):
+        self.assertEqual(self.response.status_code, 200)
+    
+    def test_template_used(self):
+        self.assertTemplateUsed(self.response, 'index.html')
