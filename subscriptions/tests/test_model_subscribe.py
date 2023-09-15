a72f92557df01b74959eebbdb1ba7aae70167157
marinacjensenn <84468498+marinacjensenn@users.noreply.github.com>
2023-09-15 14:38:41 +0000

    configuração do admin

--- subscriptions/tests/test_model_subscribe.py
+++ subscriptions/tests/test_model_subscribe.py
@@ -17,3 +17,6 @@ class SubscriptionModelTest(TestCase):
     
     def test_created_at(self):
         self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_str(self):
+        self.assertEqual('Cleber Fonseca', str(self.obj))
