d214388a2aec42d6103ec2fc16a5a7ff48477d8d
Marina Jensen <84468498+marinacjensen@users.noreply.github.com>
2023-10-20 08:07:25 -0300

    Update and rename test_view_index.py to test_view_home.py

--- core/tests/test_view_home.py
+++ core/tests/test_view_home.py
@@ -1,14 +1,16 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 class TestHome(TestCase):
     def setUp(self):
-        self.response = self.client.get('/')
+        self.response = self.client.get(r('home'))
 
     def test_home(self):
         self.assertEqual(self.response.status_code, 200)
-    
+
     def test_template_used(self):
         self.assertTemplateUsed(self.response, 'index.html')
 
     def test_subscription_link(self):
-        self.assertContains(self.response, 'href="/inscricao/"')
+        expect = 'href="{}"'.format(r('subscriptions:new'))
+        self.assertContains(self.response, expect)
