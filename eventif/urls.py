6ab151eea892c0a8ca70458e1d23cc0ac85b4ad9
Marina Jensen <84468498+marinacjensen@users.noreply.github.com>
2023-10-20 08:08:17 -0300

    Update urls.py

--- eventif/urls.py
+++ eventif/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 from core.views import home
-from subscriptions.views import subscribe
 from contact.views import contact
 
 urlpatterns = [
-    path('', home),
-    path('inscricao/', subscribe),
+    path('', home, name='home'),
+    path('inscricao/', include('subscriptions.urls')),
     path('contato/', contact),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
