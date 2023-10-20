e06c77a5be578b5a3996ac8d505913c0a67c7a52
Marina Jensen <84468498+marinacjensen@users.noreply.github.com>
2023-10-20 08:11:29 -0300

    Update and rename test_view_subscribe.py to test_view_new.py

--- subscriptions/tests/test_view_new.py
+++ subscriptions/tests/test_view_new.py
@@ -1,51 +1,51 @@
 from django.test import TestCase
+from django.core import mail
+from django.shortcuts import resolve_url as r
+
 from subscriptions.forms import SubscriptionForm
 from subscriptions.models import Subscription
-from django.core import mail
 
-class SubscribeGet(TestCase):
+class SubscriptionsNewGet(TestCase):
     def setUp(self):
-        self.response = self.client.get('/inscricao/')
+        self.response = self.client.get(r('subscriptions:new'))
 
     def test_get(self):
         """GET /inscricao/ must return status_code 200"""
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
-        """Must use subscriptions/subscription_form.html"""
+        """Must use subscriptions/subscripiton_form.html"""
         self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
 
     def test_html(self):
         """HTML must contain input tags"""
-        tags = (("<form", 1),
-                ("<input", 6),
-                ('type="text"', 3),
-                ('type="email"', 1),
-                ('type="submit"', 1))
+        tags = (
+            ('<form', 1), 
+            ('<input', 6), 
+            ('type="text"', 3), 
+            ('type="email"', 1), 
+            ('type="submit"', 1)
+        )
         for text, count in tags:
             with self.subTest():
                 self.assertContains(self.response, text, count)
-                
+
     def test_csrf(self):
-        """HTML must contain csrf"""
-        self.assertContains(self.response,  "csrfmiddlewaretoken")
+        """HTML form must contain CSRF"""
+        self.assertContains(self.response, "csrfmiddlewaretoken")
 
     def test_has_form(self):
         """Context must have subscription form"""
-        form = self.response.context["form"]
+        form = self.response.context['form']
         self.assertIsInstance(form, SubscriptionForm)
 
-class SubscribePostValid(TestCase):
+class SubscriptionsNewPostValid(TestCase):
     def setUp(self):
-        data = dict(name="Cleber Fonseca", 
-                    cpf="12345678901", 
-                    email="profcleberfonseca@gmail.com",
-                    phone="53-91234-5678")
-        
-        self.response = self.client.post('/inscricao/', data)
+        data = dict(name='Pedro Machado', cpf='12345678901', email='pedro.machado@mail.com', phone='53 91234-5678')
+        self.response = self.client.post(r('subscriptions:new'), data)
 
     def test_post(self):
-        self.assertEqual(302, self.response.status_code)
+        self.assertRedirects(self.response, r('subscriptions:detail', 1))
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -53,9 +53,9 @@ class SubscribePostValid(TestCase):
     def test_save_subscription(self):
         self.assertTrue(Subscription.objects.exists())
 
-class SubscribePostInvalid(TestCase):
+class SubscriptionsNewPostInvalid(TestCase):
     def setUp(self):
-        self.response = self.client.post('/inscricao/', {})
+        self.response = self.client.post(r('subscriptions:new'), {})
 
     def test_post(self):
         self.assertEqual(200, self.response.status_code)
@@ -70,14 +70,6 @@ class SubscribePostInvalid(TestCase):
     def test_form_has_error(self):
         form = self.response.context['form']
         self.assertTrue(form.errors)
+
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name = 'Cleber Fonseca',
-                    cpf = '12345678901',
-                    email = 'profcleberfonseca@gmail.com',
-                    phone = '53-1234-5678')
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
